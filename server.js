633475a5e03c8ca2f527ef26aef2c4c0875a210c
Fileonii <yahhoofj@gmail.com>
2020-12-24 00:42:28 +0100

    i think its finally version

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import fetch from "node-fetch";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
@@ -9,6 +12,11 @@ app.use(express.json());
 app.listen(process.env.PORT || 5000, () => {
   console.log("App is listening");
 });
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.get("/controls1", async (req, response) => {
   const api_url =
     "https://erpdemocloud.assecobs.pl/v100/homework_html/controlsTest.json";
